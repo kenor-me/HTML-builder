76000a86b4302dcdbd89d050f16e757a511a8995
Katherine Kanareikina <katyabrizitskaya@mail.ru>
2021-11-02 11:56:38 +0300

    feat: add 04-copy-directory (Tuesday, Nov 02, 2021 11:56:00)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const pathDirectory = path.join(__dirname, 'files');
+const pathCopyDir = path.join(__dirname, 'files-copy');
 
+getCurrentFilenames();
+
+const fsPromises = fs.promises;
+fsPromises.mkdir(pathCopyDir).then(function () {
+  console.log('Directory created successfully');
+}).catch(function () {
+  console.log('failed to create directory');
+});
+
+function getCurrentFilenames() {
+  fs.readdir(pathDirectory, (err, files) => {
+    files.forEach(file => {
+      fs.copyFile(`${pathDirectory}/${file}`, `${pathCopyDir}/${file}`, (err) => {
+        if (err) {
+          console.log('Error Found:', err);
+        }
+      });
+    });
+  });
+}
